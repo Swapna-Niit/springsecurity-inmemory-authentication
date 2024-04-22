4c710ce5a96122f5948e4a18306d00b790f56cc7
swapna reniguntla <swapnar.gitacc@gmail.com>
2024-04-23 00:12:21 +0530

    inmemory authentication

---
+++ src/main/java/com/crimsonlogic/springbootbasicsecurityexample_inmemory/SpringbootbasicsecurityexampleInmemoryApplication.java
@@ -0,0 +1,13 @@
+package com.crimsonlogic.springbootbasicsecurityexample_inmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicsecurityexampleInmemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicsecurityexampleInmemoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crimsonlogic/springbootbasicsecurityexample_inmemory/SpringbootbasicsecurityexampleInmemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimsonlogic.springbootbasicsecurityexample_inmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbasicsecurityexampleInmemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
